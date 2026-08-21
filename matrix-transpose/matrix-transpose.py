4df3981350fcbb9928d5443f5a05c1221aa641cb
Senthilnathan <ssenthil1490@gmail.com>
2026-08-21 13:47:04 +0530

    Solve matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -5,4 +5,10 @@ def matrix_transpose(A: list) -> np.ndarray:
     Return the transpose of matrix A (swap rows and columns).
     """
     # Write code here
-    return np.array(A).T
+    A_T = np.array(A)
+
+    if A_T.shape == (2, 2):
+        (A_T[0][1], A_T[1][0]) = (A_T[1][0], A_T[0][1])
+        return A_T
+    else:
+        return np.array(list(map(list, zip(*list(A_T)))))
